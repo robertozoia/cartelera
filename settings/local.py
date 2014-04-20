cfbf5684c49916fb38dbb7c96cf318514c94b174
Roberto Zoia <roberto.zoia@gmail.com>
2014-04-19 23:49:54 -0500

    Merged with fix settings brachn

--- settings/local.py
+++ settings/local.py
@@ -1,13 +1,17 @@
 # -*- encoding: utf-8 -*-
 
+# Configuration file for development
 
 from base import *
 
+# website root directory
 basedir = "./www"
+
+# jinja2 templates directory
 template_dir = './templates'
 
+# output webpages filenames
 bycine_file =  "%s/index.html" % basedir
-bymovie_file = "%s/bymovie.html" % basedir
 settings_file = "%s/preferences.html" % basedir
 about_file = "%s/about.html" % basedir
 
